2f90276ebba4b223531941497fe294cda0aecae7
HolyWalley <yakau@hey.com>
2025-07-26 15:33:09 +0200

    Add logo

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -4,6 +4,9 @@ import { AppSidebarMobile } from './AppSidebarMobile'
 import { useIsMobile } from '@/hooks/use-mobile'
 import { ChartNoAxesCombined, Logs, Wallet } from "lucide-react"
 import { MenuItem } from './MenuItem'
+import { Separator } from "./ui/separator"
+import Logo from '@/assets/logo.svg?react'
+import { Link } from "react-router-dom"
 
 export function AppSidebar() {
   const isMobile = useIsMobile()
@@ -15,7 +18,11 @@ export function AppSidebar() {
   return (
     <div className="fixed left-0 top-0 h-full w-24 bg-background border-r border-border flex flex-col items-center py-4 gap-4">
       {/* Content - Navigation Items */}
-      <div className="flex-1 flex flex-col gap-2">
+      <div className="flex-1 flex flex-col items-center gap-2">
+        <Link to="/dashboard">
+          <Logo className="h-8 w-8 text-foreground" />
+        </Link>
+        <Separator />
         <MenuItem to="/dashboard">
           <ChartNoAxesCombined className="h-5 w-5" />
           <span className="text-[10px]">Overview</span>
@@ -31,15 +38,17 @@ export function AppSidebar() {
           <span className="text-[10px]">Wallets</span>
         </MenuItem>
 
-        <div className="flex flex-col items-center justify-center p-2 gap-0.5">
-          <UserDropdownMenu />
-          <span className="text-[10px]">Me</span>
+        <Separator />
+
+        <div className="flex flex-col items-center justify-center">
+          <NewTransactionTrigger />
         </div>
       </div>
 
       {/* Footer - New Transaction */}
-      <div className="flex items-center justify-center">
-        <NewTransactionTrigger />
+      <div className="flex flex-col items-center justify-center gap-0.5">
+        <UserDropdownMenu />
+        <span className="text-[10px]">Me</span>
       </div>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { cloudflare } from "@cloudflare/vite-plugin"
 import mkcert from 'vite-plugin-mkcert'
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cloudflare(), mkcert()],
+  plugins: [react(), svgr(), tailwindcss(), cloudflare(), mkcert()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
