8d083da196629f5af678061a10028ee5e5916b5a
HolyWalley <yakau@hey.com>
2025-08-02 09:02:14 +0200

    Add ordering back to categories

--- src/hooks/useLiveCategories.ts
+++ src/hooks/useLiveCategories.ts
@@ -1,17 +1,17 @@
 import { useLiveQuery } from 'dexie-react-hooks'
 import { db, type DexieCategory } from '@/lib/db-dexie'
 import type { Category } from '../../shared/schemas/category.schema'
-import type { Collection, EntityTable, InsertType } from 'dexie'
 
 // TODO: create a transaction type enum-type
 
 export function useLiveCategories(type?: 'expense' | 'income' | 'transfer') {
   const categories = useLiveQuery(async () => {
-    let query: EntityTable<DexieCategory, '_id'> | Collection<DexieCategory, string, InsertType<DexieCategory, "_id">> = db.categories
+    let dexieCategories: DexieCategory[]
     if (type) {
-      query = db.categories.where('type').equals(type)
+      dexieCategories = await db.categories.where('type').equals(type).sortBy('order')
+    } else {
+      dexieCategories = await db.categories.orderBy('order').toArray()
     }
-    const dexieCategories = await query.toArray()
     // Convert Date objects back to ISO strings for components
     return dexieCategories.map(cat => ({
       ...cat,
